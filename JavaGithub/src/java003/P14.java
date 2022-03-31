520159d121f19d403f205e6e50836139c9029f0c
Jiminleeee <jiminab94@naver.com>
2022-04-01 08:40:59 +0900

    day5

--- JavaGithub/src/java003/P14.java
+++ JavaGithub/src/java003/P14.java
@@ -7,21 +7,33 @@ public class P14 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		String sentence = sc.nextLine();
-		String lowerSen = sentence.toLowerCase();
-		String target = " a";
-		int targetNum = lowerSen.indexOf(target);
-		String back = lowerSen.substring(targetNum);
-	
-		char[] vowel = {'a','e','i','o','u'};
-		int i = 0;
-		
-			if (back.charAt(2) == 'n') {
-				// 
-	
+		String text = sc.nextLine();
+		String[] strArr = text.split(" ");
+		String vowel = "aeiou";
+		String consonant = "bcdfghjklmnpqrstvwxyz";
+
+		String[] vowel2 = vowel.split("");
+		String[] consonant2 = consonant.split("");
+
+		for (int i = 1; i < strArr.length; i++) {
+			for (int j = 1; j < vowel2.length; j++) {
+				if (strArr[i].substring(0, 1).contains(vowel2[j])) {
+					if (strArr[i - 1].equals("a")) {
+						strArr[i - 1] = "an";
+					}
+				}
 			}
-		
-		System.out.println(back);	
+
+			for (int z = 0; z < consonant2.length; z++) {
+				if (strArr[i].substring(0, 1).contains(consonant2[z])) {
+					if (strArr[i - 1].equals("an")) {
+						strArr[i - 1] = "a";
+					}
+				}
+			}
+		}
+		for (int i = 0; i < strArr.length; i++) {
+			System.out.print(strArr[i] + " ");
 		}
 	}
-
+}
